87e53a5d0547e15042f397dd8334771a13fa971d
tanverrana <tanversobur@gmail.com>
2022-05-09 00:37:51 +0600

    db connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -7,6 +8,18 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ratqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log("Ts bike connected");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get("/", (req, res) => {
     res.send("Running TS Bike Server");
