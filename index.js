13f40530fefd4620e7e3b4b7260093567e04406a
tanverrana <tanversobur@gmail.com>
2022-05-09 01:01:23 +0600

    inventory with id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -23,6 +23,13 @@ async function run() {
             const query = {};
             const cursor = inventoryCollection.find(query);
             const inventory = await cursor.toArray();
+            res.send(inventory);
+        });
+
+        app.get("/inventory/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const inventory = await inventoryCollection.findOne(query);
             res.send(inventory);
         })
     }
