d34f95c2006c427236da4a960f73f62b239afdeb
tanverrana <tanversobur@gmail.com>
2022-05-09 09:27:44 +0600

    delete api create

--- index.js
+++ index.js
@@ -37,7 +37,15 @@ async function run() {
             const newInventory = req.body;
             const result = await inventoryCollection.insertOne(newInventory);
             res.send(result);
-        })
+        });
+
+        //Delete
+        app.delete("/inventory/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await inventoryCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
 
